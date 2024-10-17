9120b50fb49d8b8028f9927ac2feb59dbcd81625
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-17 20:16:39 +0300

    added select component and edited form

--- client/src/components/common/Form.jsx
+++ client/src/components/common/Form.jsx
@@ -1,11 +1,18 @@
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import PropTypes from "prop-types";
+import { Textarea } from "../ui/textarea";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "../ui/select";
 
 function CommonForm({ formControls }) {
   function renderInputsByComponentType(getControlItem) {
     let element = null;
-
     switch (getControlItem.componentType) {
       case "input":
         element = (
@@ -20,13 +27,31 @@ function CommonForm({ formControls }) {
       case "select":
         element = (
           <Select>
-            <SelectTrigger>
-              <SelectValue />
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder={getControlItem.placeholder} />
             </SelectTrigger>
+            <SelectContent>
+              {getControlItem?.options && getControlItem.options.length > 0
+                ? getControlItem.options.map((optionItem) => (
+                    <SelectItem key={optionItem.id} value={optionItem.id}>
+                      {optionItem.label}
+                    </SelectItem>
+                  ))
+                : null}
+            </SelectContent>
           </Select>
         );
         break;
       case "textarea":
+        element = (
+          <Textarea
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.id}
+          />
+        );
+        break;
+      default:
         element = (
           <Input
             name={getControlItem.name}
@@ -36,17 +61,17 @@ function CommonForm({ formControls }) {
           />
         );
         break;
-
-      default:
-        break;
     }
+
+    return element;
   }
+
   return (
     <form action="">
       <div className="flex flex-col gap-3">
         {formControls.map((controlItem) => (
           <div key={controlItem.name} className="grid w-full gap-1.5">
-            <Label className="mb-1">{controlItem.Label}</Label>{" "}
+            <Label className="mb-1">{controlItem.label}</Label>{" "}
             {renderInputsByComponentType(controlItem)}
           </div>
         ))}
