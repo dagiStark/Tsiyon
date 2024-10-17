f4fab05561f5a9323772491354afe0da5e3e5095
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-17 21:18:24 +0300

    login page completed

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,7 +1,46 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import CommonForm from "@/components/common/Form";
+import { loginFormControls } from "@/config";
+
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+};
 
 function AuthLogin() {
-  return <div>AuthLogin</div>;
+  const [formData, setFormData] = useState(initialState);
+
+  function onSubmit() {
+    // Here you can call your API to register the user
+  }
+
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Login to your account
+        </h1>
+        <p className="mt-2">
+          Don&apos;t have an account?{" "}
+          <Link
+            className="font-medium text-primary hover:underline"
+            to="/auth/register"
+          >
+            SignUp
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={loginFormControls}
+        buttonText={"Login"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
 }
 
 export default AuthLogin;
-
