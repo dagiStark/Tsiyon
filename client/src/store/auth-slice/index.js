e90a6184b37dfbdc91665cbf1df77ccb234a976c
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-18 09:34:34 +0300

    registerUser thunk (for api requests)

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -1,20 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 const initialState = {
-    isAuthenticated: false,
-    isLoading: false,
-    user: null
-}
-
+  isAuthenticated: false,
+  isLoading: false,
+  user: null,
+};
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {
-        setUser: (state, action) => {}
-    }
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {},
+  },
+});
+
+export const registerUser = createAsyncThunk(
+  "/auth/register",
+
+  async (formData) => {
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/register",
+      formData,
+      {
+        withCredentials: true,
+      }
+    );
+
+    return response.data;
+  }
+);
 
 
-export const {setUser} = authSlice.actions
-export default authSlice.reducer
+
+export const { setUser } = authSlice.actions;
+export default authSlice.reducer;
