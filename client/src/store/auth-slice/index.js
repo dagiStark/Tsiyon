0800db908db30050a6d5faff9a03d8dbea9f25f0
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-18 09:57:51 +0300

    added builder based the state

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -6,13 +6,29 @@ const initialState = {
   user: null,
 };
 
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
     setUser: (state, action) => {},
   },
+  extraReducers: (builder) => {
+    builder.addCase(registerUser.pending, (state) => {
+        state.isLoading = true
+    }).addCase(registerUser.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.user = action.payload
+        state.isAuthenticated = false
+    }).addCase(registerUser.rejected, (state) => {
+        state.isLoading = false
+        state.user = null
+        state.isAuthenticated = false
+    })
+  }
 });
+
+
 
 export const registerUser = createAsyncThunk(
   "/auth/register",
@@ -36,6 +52,8 @@ export const registerUser = createAsyncThunk(
     }
   }
 );
+
+
 
 
 export const { setUser } = authSlice.actions;
