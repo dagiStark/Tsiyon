ae467510a585c4ee8a4d7257ec7eee64a05d0ef4
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-19 21:16:16 +0300

    sidebar update

--- client/src/components/admin-view/Sidebar.jsx
+++ client/src/components/admin-view/Sidebar.jsx
@@ -1,9 +1,22 @@
+import { Fragment } from "react";
+import { ChartNoAxesCombined } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
 function AdminSidebar() {
+  const navigate = useNavigate();
   return (
-    <div>
-      AdminSidebar
-    </div>
-  )
+    <Fragment>
+      <aside className="w-64 flex-col border-r bg-background p-6">
+        <div
+          className="flex items-center gap-2 cursor-pointer"
+          onClick={() => navigate("/admin/dashboard")}
+        >
+          <ChartNoAxesCombined size={30} />
+          <h1 className="text-xl font-extrabold">Admin Panel</h1>
+        </div>
+      </aside>
+    </Fragment>
+  );
 }
 
-export default AdminSidebar
+export default AdminSidebar;
