a105c1e97a56ff88bca74eba3d7dbe139e5ab712
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-20 18:10:14 +0300

    Update Products.jsx

--- client/src/pages/admin-view/Products.jsx
+++ client/src/pages/admin-view/Products.jsx
@@ -1,15 +1,57 @@
 import { Button } from "@/components/ui/button";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import CommonForm from "@/components/common/Form";
+
+const initialFormData = {
+  image: null,
+  title: "",
+  description: "",
+  category: "",
+  brand: "",
+  price: "",
+  salePrice: "",
+  totalStock: "",
+};
 
 function AdminProducts() {
+  const [openCreateProductsDialog, setOpenCreateProductsDialog] =
+    useState(false);
+
+  const [formData, setFormData] = useState(initialFormData);
+
   return (
     <Fragment>
       <div className="mb-5 w-full flex justify-end">
-        <Button>Add New Product</Button>
+        <Button onClick={() => setOpenCreateProductsDialog(true)}>
+          Add New Product
+        </Button>
       </div>
-      <div className="grid gap-4 md:grid-cols-4 lg:grid-cols-4 ">
-        
-      </div>
+
+      <div className="grid gap-4 md:grid-cols-4 lg:grid-cols-4 "></div>
+      <Sheet
+        open={openCreateProductsDialog}
+        onOpenChange={() => setOpenCreateProductsDialog(false)}
+      >
+        <SheetContent side="right" className="overflow-auto">
+          <SheetHeader>
+            <SheetTitle>Add New Product</SheetTitle>
+          </SheetHeader>
+          <div className="py-6">
+            <CommonForm
+              formData={formData}
+              setFormData={setFormData}
+              buttonText="Add"
+              
+            />
+          </div>
+        </SheetContent>
+      </Sheet>
     </Fragment>
   );
 }
