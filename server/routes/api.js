b5621cab5dbb18dc00ce08873eb12a167f4d9b4f
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-20 21:19:39 +0300

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authRouter = require("./auth/authRoutes.js");
+const productRouter = require("./admin/producRoutes.js");
 
 router.use("/auth", authRouter);
+router.use("/admin", productRouter)
 
 module.exports = router;
