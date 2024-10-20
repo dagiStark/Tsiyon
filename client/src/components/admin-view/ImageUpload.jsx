c8c0f095b153d76a21d90266a42f5d4a93d16d06
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-20 18:53:40 +0300

    Update ImageUpload.jsx

--- client/src/components/admin-view/ImageUpload.jsx
+++ client/src/components/admin-view/ImageUpload.jsx
@@ -1,7 +1,9 @@
 import { useRef } from "react";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import PropTypes from "prop-types";
+import PropTypes, { func } from "prop-types";
+import { FileIcon, UploadCloudIcon, XIcon } from "lucide-react";
+import { Button } from "../ui/button";
 
 function AdminProductImageUpload({
   imageFile,
@@ -12,21 +14,59 @@ function AdminProductImageUpload({
   const inputRef = useRef(null);
 
   function handleImageFileChange(event) {
-    console.log(event.target.files);
+    const selectedFile = event.target.files?.[0];
+    if (selectedFile) setImageFile(selectedFile);
   }
 
+  function handleDraOver(event) {
+    event.preventDefault();
+  }
+
+  function handleDrop(event) {
+    event.preventDefault();
+    const droppedFile = event.dataTransfer.files[0];
+    if (droppedFile) setImageFile(droppedFile);
+  }
 
   return (
-    <div className="w-full max-w-md mx-auto">
+    <div className="w-full max-w-md mx-auto mt-4">
       <Label className="text-lg font-semibold mb-2 block">Upload Image</Label>
-      <div>
+      <div
+        onDragOver={handleDraOver}
+        onDrop={handleDrop}
+        className="border-2 border-dashed rounded-lg p-4 "
+      >
         <Input
           id="image-upload"
           type="file"
-          className=""
+          className="hidden"
           ref={inputRef}
           onChange={handleImageFileChange}
         />
+        {!imageFile ? (
+          <Label
+            htmlFor="image-upload"
+            className="flex flex-col items-center h-32 cursor-pointer justify-center"
+          >
+            <UploadCloudIcon className="w-10 h-10 text-muted-foreground mb-2" />
+            <span>Drag & drop or click to upload image</span>
+          </Label>
+        ) : (
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <FileIcon className="w-8 text-primary mr-2 h-8" />
+            </div>
+            <p className="text-sm font-medium">{imageFile.name}</p>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="text-muted-foreground hover:text-foreground"
+            >
+              <XIcon className="w-4 h-4" />
+              <span className="sr-only">Remove File</span>
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
