7c30886e1950162d0408ca8244db377d1f93d486
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-21 12:15:25 +0300

    Update index.js

--- client/src/store/admin/products-slice/index.js
+++ client/src/store/admin/products-slice/index.js
@@ -40,3 +40,24 @@ export const addNewProduct = createAsyncThunk(
     }
   }
 );
+
+export const fetchAllProducts = createAsyncThunk(
+  "/products/fetch-all-products",
+  async (_, { rejectWithValue }) => {
+    try {
+      const result = await axios.get(
+        "http://localhost:5000/api/admin/products/get"
+      );
+      return result?.data;
+    } catch (error) {
+      // Check if the error response contains data
+      if (error.response && error.response.data) {
+        // Return a specific error message from the server
+        return rejectWithValue(error.response.data);
+      } else {
+        // Return a general error message
+        return rejectWithValue("An error occurred while adding the product.");
+      }
+    }
+  }
+);
