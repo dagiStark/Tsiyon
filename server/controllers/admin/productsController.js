db74cdecd362ba6350b2d7c96b1493e4d9abbe47
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-21 11:17:51 +0300

    Update productsController.js

--- server/controllers/admin/productsController.js
+++ server/controllers/admin/productsController.js
@@ -1,5 +1,6 @@
-const imageUploadUtil = require("../../helpers/cloudinary").imageUploadUtil;
+const Product = require("../../models/Product");
 
+const imageUploadUtil = require("../../helpers/cloudinary").imageUploadUtil;
 
 const handleImageUpload = async (req, res) => {
   try {
@@ -7,7 +8,6 @@ const handleImageUpload = async (req, res) => {
     const url = "data:" + req.file.mimetype + ";base64," + b64;
     const result = await imageUploadUtil(url);
     console.log("RESULT: ", result);
-    
 
     res.json({
       success: true,
@@ -22,64 +22,86 @@ const handleImageUpload = async (req, res) => {
   }
 };
 
-
-
 // add a new product
 
-
-const addProduct = async(req, res) => {
+const addProduct = async (req, res) => {
   try {
-    
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    } = req.body;
+
+    const newProduct = new Product({
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    });
+
+    await newProduct.save();
+
+    res.status(201).json({
+      success: true,
+      data: newProduct,
+    });
   } catch (error) {
     res.json({
       success: false,
       message: error.message,
     });
   }
-}
+};
 
 // fetch all products
 
-const fetchProducts = async(req, res) => {
+const fetchProducts = async (req, res) => {
   try {
-    
   } catch (error) {
     res.json({
       success: false,
       message: error.message,
     });
   }
-}
-
+};
 
 // edit a product
 
-const editProduct = async(req, res) => {
+const editProduct = async (req, res) => {
   try {
-    
   } catch (error) {
     res.json({
       success: false,
       message: error.message,
     });
   }
-}
-
+};
 
 // delete a product
 
-
-const deleteProduct = async(req, res) => {
+const deleteProduct = async (req, res) => {
   try {
-    
   } catch (error) {
     res.json({
       success: false,
       message: error.message,
     });
   }
-}
+};
 
-
-
-module.exports = { handleImageUpload, addProduct, fetchProducts, editProduct, deleteProduct };
+module.exports = {
+  handleImageUpload,
+  addProduct,
+  fetchProducts,
+  editProduct,
+  deleteProduct,
+};
