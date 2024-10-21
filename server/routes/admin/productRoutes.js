c3a287186626b7d509645459f7173f240d2ae5bd
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-21 11:51:17 +0300

    Update productRoutes.js

--- server/routes/admin/productRoutes.js
+++ server/routes/admin/productRoutes.js
@@ -2,9 +2,18 @@ const express = require("express");
 const router = express.Router();
 const {
   handleImageUpload,
+  addProduct,
+  editProduct,
+  fetchProducts,
+  deleteProduct,
 } = require("../../controllers/admin/productsController");
 const upload = require("../../helpers/cloudinary").upload;
 
 router.post("/upload-image", upload.single("my_file"), handleImageUpload);
+router.post("/add", addProduct);
+router.post("/edit/:id", editProduct);
+router.get("/get", fetchProducts);
+router.post("/delete/:id", deleteProduct);
+
 
 module.exports = router;
