10fc0f571748f29e24d9169d0a652a8344880c5a
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-23 23:49:17 +0300

    Update Header.jsx

--- client/src/components/admin-view/Header.jsx
+++ client/src/components/admin-view/Header.jsx
@@ -1,8 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { logoutUser } from "@/store/auth-slice";
 import { Menu, LogOut } from "lucide-react";
 import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
 
-function AdminHeader({setOpen}) {
+function AdminHeader({ setOpen }) {
+  const dispatch = useDispatch();
+
+  function handleLogout() {
+    dispatch(logoutUser());
+  }
   return (
     <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
       <Button onClick={() => setOpen(true)} className="sm:hidden">
@@ -10,7 +17,10 @@ function AdminHeader({setOpen}) {
         <span className="sr-only">Toggle Menu</span>
       </Button>
       <div className="flex flex-1 justify-end ">
-        <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow">
+        <Button
+          className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow"
+          onClick={() => handleLogout()}
+        >
           <LogOut />
           Logout
         </Button>
@@ -22,5 +32,5 @@ function AdminHeader({setOpen}) {
 export default AdminHeader;
 
 AdminHeader.propTypes = {
-  setOpen: PropTypes.func.isRequired
-}
+  setOpen: PropTypes.func.isRequired,
+};
