9b63a6fd5f413e6b698d36f2221edf8e6090c469
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-24 21:06:11 +0300

    Update ProductTile.jsx

--- client/src/components/shopping-view/ProductTile.jsx
+++ client/src/components/shopping-view/ProductTile.jsx
@@ -1,4 +1,7 @@
-import { Card } from "../ui/card";
+import { Badge } from "../ui/badge";
+import { Card, CardContent } from "../ui/card";
+import PropTypes from "prop-types";
+
 
 function ShoppingProductTile({ product }) {
   return (
@@ -10,13 +13,38 @@ function ShoppingProductTile({ product }) {
             alt={product?.title}
             className="w-full h-[300px] object-cover rounded-t-lg"
           />
-          {
-            product?.salePrice > 0 ?
-          }
+          {product?.salePrice > 0 ? (
+            <Badge
+              className={"absolute top-2 left-2 bg-red-500 hover:bg-red-600"}
+            >
+              Sale
+            </Badge>
+          ) : null}
         </div>
+        <CardContent className="p-4">
+          <h2 className="text-xl font-bold mb-2">{product?.title}</h2>
+          <div className="flex items-center justify-between mb-2">
+            <span className="text-sm text-muted-foreground">
+              {product?.category}
+            </span>
+            <span className="text-sm text-muted-foreground">
+              {product?.brand}
+            </span>
+          </div>
+        </CardContent>
       </div>
     </Card>
   );
 }
 
 export default ShoppingProductTile;
+
+ShoppingProductTile.propTypes = {
+  product: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    brand: PropTypes.string.isRequired,
+    salePrice: PropTypes.number,
+  }).isRequired,
+};
