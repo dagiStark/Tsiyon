a9609eafa6205ef557c5fbd51eec78370d02859b
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-24 20:34:19 +0300

    Update Listing.jsx

--- client/src/pages/shopping-view/Listing.jsx
+++ client/src/pages/shopping-view/Listing.jsx
@@ -1,9 +1,42 @@
 import ProductFilter from "@/components/shopping-view/Filter";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ArrowUpDown } from "lucide-react";
 
 function ShoppingListing() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
       <ProductFilter />
+      <div className="bg-background w-full rounded-lg shadow-sm">
+        <div className="p-4 border-b flex items-center justify-between">
+          <h2 className="text-lg font-extrabold">All Products</h2>
+          <div className="flex items-center gap-3">
+            <span className="text-muted-foreground">10 Products</span>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  className="flex items-center gap-1"
+                  size="sm"
+                >
+                  <ArrowUpDown className="h-4 w-4" />
+                  <span>Sort by</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="">
+                <DropdownMenuRadioGroup>
+
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
