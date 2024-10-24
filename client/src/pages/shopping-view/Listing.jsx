8900177c2133b2c712583b791b2f311ef96194bf
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-24 15:26:25 +0300

    create filter and included in listing

--- client/src/pages/shopping-view/Listing.jsx
+++ client/src/pages/shopping-view/Listing.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import ProductFilter from "@/components/shopping-view/Filter";
 
 function ShoppingListing() {
-  return <div>shopping listing</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
+      <ProductFilter />
+    </div>
+  );
 }
 
 export default ShoppingListing;
