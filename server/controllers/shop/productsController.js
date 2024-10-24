9f84a1d52d9dfd06f64bcf9f7bbcdf7a491d733e
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-10-24 22:34:39 +0300

    Create productsController.js

---
+++ server/controllers/shop/productsController.js
@@ -0,0 +1,19 @@
+const Product = require("../../models/Product");
+
+const getFilteredProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({
+      success: true,
+      data: products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+module.exports = { getFilteredProducts };
